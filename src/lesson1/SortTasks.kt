d6ba2cdbb1748f80eafd48099a94a27b53446813
prdumbledore <vjqljv29>
2020-10-08 20:52:04 +0300

    lesson 1

--- src/lesson1/SortTasks.kt
+++ src/lesson1/SortTasks.kt
@@ -2,6 +2,11 @@
 
 package lesson1
 
+import java.io.File
+import java.io.FileWriter
+import java.lang.StringBuilder
+import kotlin.math.abs
+
 /**
  * Сортировка времён
  *
@@ -31,9 +36,33 @@ package lesson1
  * 07:56:14 PM
  *
  * В случае обнаружения неверного формата файла бросить любое исключение.
+ * время: O(N*logN)
+ * память: O(N)
  */
 fun sortTimes(inputName: String, outputName: String) {
-    TODO()
+    val sortedFile = StringBuilder()
+    val amTime = mutableListOf<String>()
+    val pmTime = mutableListOf<String>()
+    for (timeLine in File(inputName).readLines()) {
+        require(Regex("""^(0[1-9]|1[0-2]):([0-5]\d):([0-5]\d)\s[AP]M$""").matches(timeLine))
+        var time = timeLine
+        if (time.startsWith("12")) time = time.replaceFirst("12", "00")
+        if (time.endsWith("AM")) amTime.add(time)
+        else pmTime.add(time)
+    }
+    amTime.sort()
+    pmTime.sort()
+    for (timeLine in amTime) {
+        if (timeLine.startsWith("00")) sortedFile.append(timeLine.replaceFirst("00", "12"))
+        else sortedFile.append(timeLine)
+        sortedFile.append(System.lineSeparator())
+    }
+    for (timeLine in pmTime) {
+        if (timeLine.startsWith("00")) sortedFile.append(timeLine.replaceFirst("00", "12"))
+        else sortedFile.append(timeLine)
+        sortedFile.append(System.lineSeparator())
+    }
+    File(outputName).writeText(sortedFile.toString())
 }
 
 /**
@@ -61,9 +90,31 @@ fun sortTimes(inputName: String, outputName: String) {
  * Садовая 5 - Сидоров Петр, Сидорова Мария
  *
  * В случае обнаружения неверного формата файла бросить любое исключение.
+ * Время:O(N*logN)
+ * Память:O(N)
  */
 fun sortAddresses(inputName: String, outputName: String) {
-    TODO()
+    val addressBook = mutableMapOf<String, MutableSet<String>>()
+    for (line in File(inputName).readLines()) {
+        require(Regex("""^[\wа-яА-ЯЁё]+\s[\wа-яА-ЯЁё]+\s-\s[\wа-яА-ЯЁё-]+\s\d+$""").matches(line))
+        val list = line.split(" - ")
+        if (!addressBook.containsKey(list[1])) addressBook[list[1]] = mutableSetOf()
+        addressBook[list[1]]!!.add(list[0])
+    }
+    val sb = StringBuilder()
+    addressBook.keys.sortedWith(
+        compareBy(
+            { it.substringBeforeLast(" ") },
+            { it.substringAfterLast(" ").toInt() }
+        )
+    )
+        .forEach {
+            sb.append(it).append(" - ")
+            sb.append(addressBook[it]!!.sorted().joinToString(separator = ", "))
+            sb.append(System.lineSeparator())
+        }
+
+    File(outputName).writeText(sb.toString())
 }
 
 /**
@@ -95,10 +146,40 @@ fun sortAddresses(inputName: String, outputName: String) {
  * 24.7
  * 99.5
  * 121.3
+ *
+ * Память: O(N)
+ * Время: O(N)
  */
 fun sortTemperatures(inputName: String, outputName: String) {
-    TODO()
+    val inputTemperature = mutableListOf<Int>()
+
+    var max = 5001
+    var min = -2731
+
+    for (line in File(inputName).readLines()) {
+        inputTemperature.add(line.replace(".", "").toInt())
+        if (inputTemperature[inputTemperature.size - 1] < min || min == -2731)
+            min = inputTemperature[inputTemperature.size - 1]
+        if (inputTemperature[inputTemperature.size - 1] > max || max == 5001)
+            max = inputTemperature[inputTemperature.size - 1]
+    }
+
+    val sortTemp = IntArray(max - min + 1)
+
+    for (i in 0..(max - min)) sortTemp[i] = 0
+
+    for (temp in inputTemperature) sortTemp[temp - min]++
+
+    File(outputName).bufferedWriter().use {
+        for (i in 0..(max - min)) {
+            for (j in 0 until sortTemp[i]) {
+                it.write(((i + min) / 10.0).toString())
+                it.newLine()
+            }
+        }
+    }
 }
+
 
 /**
  * Сортировка последовательности
